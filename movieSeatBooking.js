0ab6a583e8f3c4fa90ce25c372a342bd1d6646fd
rakeshbodavula <78262219+rakeshbodavula@users.noreply.github.com>
2022-05-24 22:18:30 +0530

    Add files via upload

---
+++ movieSeatBooking.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    const seats = $('.seat')
+    let cost = $('#movie').value
+    
+    function updateCount(){
+        let count = $('.row .seat.selected')
+        let seatsCount = count.length
+        $('.count').html(seatsCount)
+        $('.cost').html(seatsCount*cost)
+    }
+    
+
+    $('#movie').on('change',(e)=>{
+        cost = +e.target.value
+        updateCount()
+    })
+    
+    
+    $('.seat').on('click',(e)=>{
+        if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
+            if(cost === undefined){
+                alert('Please select a movie')
+                return;
+            }
+            e.target.classList.toggle('selected')
+            updateCount();
+        }
+    })
+})
